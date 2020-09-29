d7a89eedc67375fe0dc6fd9f3d4169c1ddb0b5b8
Bryan Tumbaco Duarte <btumbacod@est.ups.edu.ec>
2020-09-29 14:25:52 -0500

    Actualizacion

---
+++ code/node_project/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const response = require('/network/response')
+
+const router = require('/network/routes')
+
+var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+router(app)
+
+
+
+app.use('/', express.static('public'))
+
+app.listen(5000)
+console.log('La aplicacion esta escuchando en: ')
+
